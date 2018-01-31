e9be451f0274af7a086c721a3b35c35c39e8dc52
root <root@suravitc.c.seraphic-scarab-178314.internal>
2018-01-31 03:26:18 +0000

    second

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'unique');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Spiral29');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
