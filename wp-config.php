5f724efd8f2b9e63f861dd72413e822cce4984d3
Suravit <suravit@madappe.com>
2018-03-07 17:19:25 +0700

    include backup

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'unique');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Spiral29');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
